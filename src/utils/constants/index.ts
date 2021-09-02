19178578ba93cf28a1912122838b8cc571c3e9f3
dongwonnn <kkum9408@gmail.com>
2021-09-02 10:51:16 +0900

    Deploy: heroku 이용해 json-server 배포

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -4,7 +4,7 @@ export const COMPLETE_ERROR_MESSAGE = "체크 중 오류가 발생했습니다."
 export const REMOVE_ERROE_MESSAGE = "삭제 중 오류가 발생했습니다.";
 export const CREATE_ERROE_MESSAGE = "생성 중 오류가 발생했습니다.";
 
-export const BASE_URL = "https://paywork-dw.herokuapp.com//";
+export const BASE_URL = "https://paywork-dw.herokuapp.com/";
 export const END_POINT = "todos";
 
 export const DATE_FORM: Intl.DateTimeFormatOptions = {
