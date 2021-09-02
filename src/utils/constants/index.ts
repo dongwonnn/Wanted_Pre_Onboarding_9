c590b93585606af14ca9103996e1add4db2ab4cf
dongwonnn <kkum9408@gmail.com>
2021-09-02 11:27:11 +0900

    Test: 배포 테스트

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,7 +1,7 @@
 export const INPUT_ERROR_MESSAGE = "할 일을 입력해주세요.";
 export const READ_ERROR_MESSAGE = "데이터를 읽지 못했습니다.";
 export const COMPLETE_ERROR_MESSAGE = "체크 중 오류가 발생했습니다.";
-export const REMOVE_ERROE_MESSAGE = "삭제 중 오류가 발생했습니다.";
+export const REMOVE_ERROE_MESSAGE = "삭제 중 오류가 발생했습니다. ";
 export const CREATE_ERROE_MESSAGE = "생성 중 오류가 발생했습니다.";
 
 export const BASE_URL = "https://paywork-dw.herokuapp.com/";
